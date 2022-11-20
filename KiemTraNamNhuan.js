892d55a6f54afb0ed9485e42771fdbde9323ddbc
PhapKhuong <phapkhuong@gmail.com>
2022-11-21 02:59:47 +0700

    Update KiemTraNamNhuan.js

--- KiemTraNamNhuan.js
+++ KiemTraNamNhuan.js
@@ -1,27 +1,22 @@
 function testLeapYear()
     {
         let input = +document.getElementById("year").value;
+        let isLeapYear = false;
 
         if (input % 4 === 0)
             {
-                if (input % 100 !== 0)
-                    {
-                        document.getElementById('result').innerText = "Kết quả: " + input + " is leap year!";
-                    }
-                else if (input % 100 === 0)
+                if (input % 100 === 0)
                     {
                         if (input % 400 !== 0)
                             {
-                                document.getElementById('result').innerText = "Kết quả: " + input + " is not leap year!";
-                            }
-                        else
-                            {
-                                document.getElementById('result').innerText = "Kết quả: " + input + " is leap year!";
+                                isLeapYear = true;
                             }
                     }
             }
+
+        if (isLeapYear)
+            {document.getElementById('result').innerText = "Kết quả: " + input + " is not leap year!";}
         else
-            {
-                document.getElementById('result').innerText = "Kết quả: " + input + " is not leap year!";
-            }
+            {document.getElementById('result').innerText = "Kết quả: " + input + " is leap year!";}
+
     }
